c4cb56d3cba5d2b417ecec2f3e5c9acc361c1247
erikamorais <erikamorais2001@gmail.com>
2018-08-07 14:54:41 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
-def say_hello (your_name="Bob")
-  puts "Hello #{your_name}!"
+def say_hello (greeting="Hi", your_name="Bob")
+  puts "#{greeting} #{your_name}!"
 end
 
-say_hello("Erika")
+say_hello("Hola", "Erika")
 say_hello()
